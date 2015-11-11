af5f97404c8d7189ab324d8f03bc2d005cc67f4d
Blomr <remcoblom92@hotmail.com>
2015-11-11 15:31:55 +0100

    RandomSwapper aanpassing + ChunkSwap toegevoegd

---
+++ Fruitvliegen_ChunkSwap.py
@@ -0,0 +1,44 @@
+# The next algorithm searches for the lowest number in the array (1).
+# When it finds the number it inverses all numbers from the most left till the number itself.
+# Now the numbers that are swapped are mirrored compared to their previous state.
+# 1 is now in the right position. Now the algorithm searches for 2.
+# When it is found, it repeats the process except that 1 stays in place.
+# The algorithm is done when the whole array is sorted.
+
+melano = [23, 1, 2, 11, 24, 22, 19, 6, 10, 7, 25, 20, 5, 8, 18, 12, 13, 14, 15, 16, 17, 21, 3, 4, 9]
+swaps = 0
+swapLengthTotal = 0
+
+noLast = []
+
+for i in range(len(melano)):
+	if i == 24:
+		break
+	if melano[i + 1] == melano[i] + 1:
+		print str(melano[i]) + " - " + str(melano[i + 1])
+		noLast.append(melano[i])
+	if melano[i + 1] == melano[i] - 1:
+		print str(melano[i]) + " - " + str(melano[i + 1])
+		noLast.append(melano[i])
+
+print noLast
+
+"""#iterate over all numbers
+for i in range(len(melano)):
+	print "Replace: " + str(i + 1)
+
+	# if number is already in position, continue
+	if i + 1 == melano[i]:
+		print "NO SWAP!\n"
+		continue
+		
+	# if not, swap part of the list
+	swapLengthTotal += len(melano[i:melano.index(i + 1) + 1])
+	print "Swap length: " + str(len(melano[i:melano.index(i + 1) + 1]))
+	melano[i:melano.index(i + 1) + 1] = reversed(melano[i:melano.index(i + 1) + 1])
+	swaps = swaps + 1
+	print str(melano) + "\n"
+	
+print "---> Swaps: " + str(swaps)
+print "---> Total swap length: " + str(swapLengthTotal)
+print "---> Average: " + str(swapLengthTotal / swaps) + " genes per swap"""
--- RandomSwapper.py
+++ RandomSwapper.py
@@ -1,14 +1,20 @@
 from random import randint
 
 melano = [23, 1, 2, 11, 24, 22, 19, 6, 10, 7, 25, 20, 5, 8, 18, 12, 13, 14, 15, 16, 17, 21, 3, 4, 9]
+miranda = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
 
-for i in range(5):
+counter = 0
+
+while melano != miranda:
 
 	j = randint(0,23)
-	k = randint(j + 1, 25)
+	k = randint(j + 2, 25)
 
-	print "Swap #" + str(i + 1)
+	print "Swap #" + str(counter + 1)
 	print "Swap length: " + str(len(melano[j:k]))
 	melano[j:k] = reversed(melano[j:k])
 	print str(melano) + "\n"
+	counter = counter + 1
+	
+print "Swaps: " + str(counter) + "\n"
 	
