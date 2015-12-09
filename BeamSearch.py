32ff8285b0ebc84e3c9361ee89e5d5449a7f2bab
NielsPann <niels.pannekeet@hva.nl>
2015-12-09 21:04:00 +0100

    improved beamsearch

--- BeamSearch.py
+++ BeamSearch.py
@@ -6,41 +6,47 @@ def beamSearch(alist):
     queue = []
     count = 0
     for i in range(len(alist)):
-        #print(i)
         for j in range(i + 1, len(alist)):
             blist = deepcopy(alist)
             blist[i:j+1] = reversed(blist[i:j+1])
-            #print(blist)
             queue.append(blist)
             count += 1     
     return queue
       
 def chunkFinder(alist):
-    count = 0
-    chunk = 0
+    counter = 0
+    elements = 0
+    chunklength = 0
+    score = 0
     biggestChunk = 0
-    blist = [None]*len(alist)
-    
     
     for i in range(len(alist)):
         #detects if numbers are already sorted and counts those numbers
         #here we call those numbers sorted numbers a chunk
         if alist[(i+1)% len(alist)] == alist[i]+1 or alist[(i-1)% len(alist)] == alist[i]-1:
-            #represents te size of the chunk
-            chunk += 1
-            #optional, just to show the chunks in an empty list
-            blist[i] = alist[i]
-            
-            #gives end and begin index of chunk
-            if alist[(i+1)% len(alist)] != alist[i]+1:
-                beginChunk = alist.index(alist[i])-chunk+1
-                endChunk = alist.index(alist[i])
-                #print("chunsize is",chunk, "begin index chunk is",beginChunk,"end index chunk is", endChunk)
-                # reset chunk size for next chunk
-                if chunk > biggestChunk:
-                    biggestChunk = chunk
-                chunk = 0
-    return biggestChunk
+            #represents the total length of the chunks
+            chunklength += 1
+            counter += 1
+            #check if end of chunk is reached and ads an extra number to the counter.
+            # this to count the chunk as one element after distracting the size from the counter.
+            if alist[(i-1)% len(alist)] == alist[i]-1 and alist[(i+1)% len(alist)] != alist[i]+1:
+                counter += 1
+                
+        #same as before, but this one counts the chunks in a negative order
+        elif alist[(i+1)% len(alist)] == alist[i]-1 or alist[(i-1)% len(alist)] == alist[i]+1:
+            chunklength += 1
+            counter += 1
+            #check if end of chunk is reached and ads an extra number to the counter.
+            # this to count the chunk as one element after distracting the size from the counter.
+            if alist[(i-1)% len(alist)] == alist[i]+1 and alist[(i+1)% len(alist)] != alist[i]-1:
+                counter += 1
+        #update counter after finding a single number
+        else:
+            counter += 1
+
+    elements = counter - chunklength
+    score = chunklength / elements
+    return score
 
 def threeLayerSearch(layerOne):
     countk = 0
@@ -49,30 +55,34 @@ def threeLayerSearch(layerOne):
     repLayerOne = []
     repLayerTwo = []
     repLayerTrhee = []
-    longestChunk = 0
-    currentChunk = 0
+    highestScore = 0
+    currentScore = 0
     for i in layerOne:
         templayerTwo = beamSearch(i)   
         for j in templayerTwo:
             tempLayerThree = beamSearch(j)
             for m in tempLayerThree:
                 countk += 1
-                # saves the first instance of the longest chunk
-                currentChunk = chunkFinder(m)
-                if currentChunk > longestChunk:
-                    longestChunk = currentChunk
+                # saves the best out of 27000000 
+                currentScore = chunkFinder(m)
+                if currentScore > highestScore:
+                    highestScore = currentScore
                     repLayerOne = i
                     repLayerTwo = j
                     repLayerTrhee = m
     #print(tempLayerTwo)
     print(countk)
-    print("total adjecent numbers is", longestChunk)
+    print("current score is", highestScore)
     print("\n",repLayerOne,"\n",repLayerTwo, "\n", repLayerTrhee, "chosen out of 27 000 000")
     return repLayerTrhee
 
     
 alist = [23,1,2,11,24,22,19,6,10,7,25,20,5,8,18,12,13,14,15,16,17,21,3,4,9]
-layerOne = beamSearch(alist)
+blist = [23, 1, 2, 11, 10, 7, 6, 19, 22, 24, 25, 20, 5, 4, 3, 21, 17, 16, 15, 14, 13, 12, 18, 8, 9]
+clist = [12, 13, 14, 15, 16, 17, 21, 3, 4, 5, 6, 7, 10, 11, 2, 1, 23, 22, 24, 25, 20, 19, 18, 8, 9]
+dlist = [25, 24, 22, 23, 1, 2, 3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 15, 16, 17, 21, 20, 19, 18, 8, 9]
+elist = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 25, 24, 22, 23, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+layerOne = beamSearch(elist)
 layerThree = threeLayerSearch(layerOne)
 
 
